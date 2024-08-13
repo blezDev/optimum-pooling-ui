ae501fe2d7051d6ad24bb188fef084d1c6437ba8
Ashish.Satapathy <ashish.satapathy@itcinfotech.com>
2024-08-13 05:31:28 +0000

    Added UIs

---
+++ src/app/booking-confirm/booking-confirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-confirm.component.html',
+  styleUrls: ['./booking-confirm.component.css']
+})
+export class BookingConfirmComponent {
+
+}
